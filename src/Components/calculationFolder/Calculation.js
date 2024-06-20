b30b34b5ac7e11cacd108395941a6743a6336bcb
abdul-aual <mdabdulaual38@gmail.com>
2024-06-21 01:59:16 +0600

    hello

--- src/Components/calculationFolder/Calculation.js
+++ src/Components/calculationFolder/Calculation.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculation from './calculation.css';
 const Calculation = (props) => {
+    const [num, setNum]=useState('');
     const{id,name,image,phone,email,salary} = props.allDetails;
+    const showMobile=()=>{
+        setNum(phone);
+    }
     return (
         <div>
              <div>
@@ -14,9 +18,10 @@ const Calculation = (props) => {
                     <div className='hello2'>
                         <div className='details-div'>
                             <h2>{name}</h2>
-                            <p>Phone: {phone}</p>
+                            <p>Phone: {num}</p>
                             <p>Email: {email}</p>
                             <h4>Salary: ${salary}</h4>
+                            <button onClick={showMobile} >Show Phone Number</button>
                             <button onClick={()=>props.handleAddCart(props.allDetails)} >Add User</button>
                         </div>
                     </div>
